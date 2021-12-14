0da2dab8f1da9c61cf87621e8afdac68287a6260
Gregory Koh <pyroflame11@gmail.com>
2021-12-14 14:24:41 +0800

    Add Auth Service

---
+++ src/main/java/com/gkwc/simpleblog/service/AuthService.java
@@ -0,0 +1,29 @@
+package com.gkwc.simpleblog.service;
+
+import com.gkwc.simpleblog.dto.RegisterRequest;
+import com.gkwc.simpleblog.model.User;
+import com.gkwc.simpleblog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUserName(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
+
+        userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+}
