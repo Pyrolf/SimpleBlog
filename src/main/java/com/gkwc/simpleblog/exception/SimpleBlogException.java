ac023fe911780ffda9e23ea904d9b56491bae0a0
Gregory Koh <pyroflame11@gmail.com>
2021-12-17 13:58:28 +0800

    Add JWT

---
+++ src/main/java/com/gkwc/simpleblog/exception/SimpleBlogException.java
@@ -0,0 +1,7 @@
+package com.gkwc.simpleblog.exception;
+
+public class SimpleBlogException extends RuntimeException {
+    public SimpleBlogException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/gkwc/simpleblog/security/JwtProvider.java
+++ src/main/java/com/gkwc/simpleblog/security/JwtProvider.java
@@ -1,8 +1,18 @@
 package com.gkwc.simpleblog.security;
 
-import java.security.Key;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 
 import javax.annotation.PostConstruct;
+
+import com.gkwc.simpleblog.exception.SimpleBlogException;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
@@ -10,31 +20,57 @@ import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtProvider {
-    private Key key;
+    private KeyStore keyStore;
 
     @PostConstruct
     public void init() {
-        key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        try {
+            keyStore = KeyStore.getInstance("JKS");
+            InputStream resourceAsStream = getClass().getResourceAsStream("/simpleblog.jks");
+            keyStore.load(resourceAsStream, "Passw0rd!".toCharArray());
+        } catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | IOException e) {
+            throw new SimpleBlogException("Exception occured while loading keystore");
+        }
     }
 
     public String generateToken(Authentication authentication) {
         User principal = (User) authentication.getPrincipal();
-        return Jwts.builder().setSubject(principal.getUsername()).signWith(key)
+        return Jwts.builder()
+                .setSubject(principal.getUsername())
+                .signWith(getPrivateKey())
                 .compact();
     }
 
+    private PrivateKey getPrivateKey() {
+        try {
+            return (PrivateKey) keyStore.getKey("simpleblog", "Passw0rd!".toCharArray());
+        } catch (UnrecoverableKeyException | KeyStoreException | NoSuchAlgorithmException e) {
+            throw new SimpleBlogException("Exception occured while retrieving private key from keystore");
+        }
+    }
+
     public boolean validateToken(String jwt) {
-        Jwts.parser().setSigningKey(key).parseClaimsJws(jwt);
+        Jwts.parser().setSigningKey(getPublicKey()).parseClaimsJws(jwt);
         return true;
     }
 
+    private PublicKey getPublicKey() {
+        try {
+            return keyStore.getCertificate("simpleblog").getPublicKey();
+        } catch (KeyStoreException e) {
+            throw new SimpleBlogException("Exception occured while retrieving public key from keystore");
+        }
+    }
+
     public String getUsernameFromJWT(String token) {
-        Claims claims = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+        Claims claims = Jwts.parser()
+                .setSigningKey(
+                        getPublicKey())
+                .parseClaimsJws(token)
+                .getBody();
         return claims.getSubject();
     }
 }
